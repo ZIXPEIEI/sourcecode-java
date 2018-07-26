8f2ab5cf3b15ab3ac326cb9a36231d00c4cfd983
ZIXPEIEI <tarwannamtan@gmail.com>
2018-07-26 17:13:48 +0700

    Array

---
+++ whatever/src/WorkingWithArray.java
@@ -0,0 +1,11 @@
+
+public class WorkingWithArray {
+
+    public static void main(String[] args) {
+        int[] number2 = new int[5];
+        for (int i : number2) {
+            System.out.println(i);
+        }
+    }
+
+}
--- whatever/src/WorkingWithStringLoop.java
+++ whatever/src/WorkingWithStringLoop.java
@@ -2,8 +2,18 @@
 public class WorkingWithStringLoop {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+       String name = "Tutpoom2018";
+       
+       for (;name.length()>0;) {
+           //System.out.println(name.charAt(i));
+           System.out.println(name.substring(0,1));
+           name = name.substring(1);
+           System.out.print(name);
+    }
     }
 
 }
+
+
+
+
